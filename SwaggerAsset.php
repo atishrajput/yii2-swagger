842a5fc0ef7eb717dc0e0bb39a11f314b23a2703
Atish Kumar <atikumar@cisco.com>
2021-04-29 14:53:16 +0530

    Yii2 Swagger Extension;

---
+++ SwaggerAsset.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace atishrajput\Yii2Swagger;
+
+use yii\web\AssetBundle;
+
+/**
+ * Class SwaggerAsset
+ *
+ * @package atishrajput\Yii2Swagger
+ */
+class SwaggerAsset extends AssetBundle
+{
+    /**
+     * @var string the directory that contains the source asset files for this asset bundle
+     */
+    public $sourcePath = '@bower/swagger-ui/dist';
+
+    /**
+     * @var array list of JavaScript files that this bundle contains
+     */
+    public $js = [
+        'swagger-ui-bundle.js',
+        'swagger-ui-standalone-preset.js',
+    ];
+
+    /**
+     * @var array list of CSS files that this bundle contains
+     */
+    public $css = [
+        'swagger-ui.css',
+    ];
+}
